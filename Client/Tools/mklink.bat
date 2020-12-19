46831093a03edb66d905d6ec4106c39f55565eac
sy545633799 <545633799@qq.com>
2020-12-19 21:05:11 +0800

    超链接

---
+++ Client/Tools/mklink.bat
@@ -0,0 +1,35 @@
+﻿@echo off
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+
+echo permission...
+
+goto UACPrompt
+
+) else ( goto gotAdmin )
+
+:UACPrompt
+
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+"%temp%\getadmin.vbs"
+
+exit /B
+
+:gotAdmin
+
+
+set targetName=client_lnk
+set targetDir=%~dp0\..\..\%targetName%
+set sourceDir=%~dp0\..
+
+mkdir %targetDir%
+mklink /d %targetDir%\Assets  %sourceDir%\Assets
+mklink /d %targetDir%\Library  %sourceDir%\Library
+mklink /d %targetDir%\ProjectSettings  %sourceDir%\ProjectSettings
+mklink /d %targetDir%\Packages  %sourceDir%\Packages
+pause
